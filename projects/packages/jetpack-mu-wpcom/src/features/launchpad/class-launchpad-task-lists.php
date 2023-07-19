f080f853f8fbf417d396a472e7786a241a3dc376
Valter Lorran <valter.lorran@automattic.com>
2023-07-19 16:33:31 -0300

    Apply feedback

--- projects/packages/jetpack-mu-wpcom/src/features/launchpad/class-launchpad-task-lists.php
+++ projects/packages/jetpack-mu-wpcom/src/features/launchpad/class-launchpad-task-lists.php
@@ -292,9 +292,14 @@ class Launchpad_Task_Lists {
 		$built_task['badge_text']   = $this->load_value_from_callback( $task, 'badge_text_callback' );
 		$built_task['isLaunchTask'] = isset( $task['isLaunchTask'] ) ? $task['isLaunchTask'] : false;
 
-		$repetition_count                 = $this->load_repetition_count( $task );
-		$built_task['repetition_count']   = $repetition_count ? $repetition_count : null;
-		$built_task['minimum_repetition'] = isset( $task['minimum_repetition_to_complete'] ) ? $task['minimum_repetition_to_complete'] : null;
+		if ( isset( $task['target_repetitions'] ) ) {
+			$built_task['target_repetitions'] = $task['target_repetitions'];
+			$repetition_count                 = $this->load_repetition_count( $task );
+			$built_task['repetition_count']   = $repetition_count;
+		} else {
+			$built_task['target_repetitions'] = null;
+			$built_task['repetition_count']   = null;
+		}
 
 		return $built_task;
 	}
@@ -604,10 +609,10 @@ class Launchpad_Task_Lists {
 			return false;
 		}
 
-		$has_both_properties_for_counting = ( isset( $task['minimum_repetition_to_complete'] ) || isset( $task['repetition_count_callback'] ) ) && ! ( isset( $task['minimum_repetition_to_complete'] ) && isset( $task['repetition_count_callback'] ) );
+		$missing_repepetition_property = ( isset( $task['target_repetitions'] ) || isset( $task['repetition_count_callback'] ) ) && ! ( isset( $task['target_repetitions'] ) && isset( $task['repetition_count_callback'] ) );
 
-		if ( $has_both_properties_for_counting ) {
-			_doing_it_wrong( 'validate_task', 'The Launchpad task being registered requires both a "minimum_repetition_to_complete" attribute and a "repetition_count_callback" callback', '6.3' );
+		if ( $missing_repepetition_property ) {
+			_doing_it_wrong( 'validate_task', 'The Launchpad task being registered requires both a "target_repetitions" attribute and a "repetition_count_callback" callback', '6.3' );
 			return false;
 		}
 
--- projects/packages/jetpack-mu-wpcom/tests/php/features/launchpad/class-launchpad-task-lists-test.php
+++ projects/packages/jetpack-mu-wpcom/tests/php/features/launchpad/class-launchpad-task-lists-test.php
@@ -179,38 +179,38 @@ class Launchpad_Task_Lists_Test extends \WorDBless\BaseTestCase {
 	}
 
 	/**
-	 * Test the validation of the repetition props. Both properties(minimum_repetition_to_complete & repetition_count_callback)
+	 * Test the validation of the repetition props. Both properties(target_repetitions & repetition_count_callback)
 	 * must be set if one is set.
 	 */
 	public function test_repetition_counting_task_definition() {
 		$valid_task_list = array(
-			'id'                             => 'task_0',
-			'title'                          => 'task_0',
-			'minimum_repetition_to_complete' => 2,
-			'repetition_count_callback'      => function () {
+			'id'                        => 'task_0',
+			'title'                     => 'task_0',
+			'target_repetitions'        => 2,
+			'repetition_count_callback' => function () {
 				return 1;
 			},
 		);
 		$this->assertTrue( Launchpad_Task_Lists::validate_task( $valid_task_list ) );
 
 		$valid_task_list = array(
-			'id'                             => 'task_0',
-			'title'                          => 'task_0',
-			'minimum_repetition_to_complete' => 2,
+			'id'                 => 'task_0',
+			'title'              => 'task_0',
+			'target_repetitions' => 2,
 		);
 		$this->assertFalse( Launchpad_Task_Lists::validate_task( $valid_task_list ) );
 	}
 
 	/**
-	 * Test the values for minimum_repetition_to_complete and repetition_count_callback.
+	 * Test the values for target_repetitions and repetition_count_callback.
 	 */
 	public function test_repetition_counting_task_list() {
 		wpcom_register_launchpad_task(
 			array(
-				'id'                             => 'task-0',
-				'title'                          => 'Task 0',
-				'minimum_repetition_to_complete' => 2,
-				'repetition_count_callback'      => function () {
+				'id'                        => 'task-0',
+				'title'                     => 'Task 0',
+				'target_repetitions'        => 2,
+				'repetition_count_callback' => function () {
 					return 1;
 				},
 			)
@@ -235,6 +235,6 @@ class Launchpad_Task_Lists_Test extends \WorDBless\BaseTestCase {
 		$first_task = wpcom_get_launchpad_checklist_by_checklist_slug( 'task-list-test' )[0];
 
 		$this->assertSame( 1, $first_task['repetition_count'] );
-		$this->assertEquals( 2, $first_task['minimum_repetition'] );
+		$this->assertEquals( 2, $first_task['target_repetitions'] );
 	}
 }
